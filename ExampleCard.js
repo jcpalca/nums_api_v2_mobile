4628b0719710d2f954554a63b92ff6e065625c5d
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2023-01-17 12:28:58 -0800

    Change style

--- ExampleCard.js
+++ ExampleCard.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import { View, Text, StyleSheet, Dimensions, Image } from "react-native"
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 
-export const SLIDER_WIDTH = Dimensions.get('window').width + 80
-export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7)
+export const SLIDER_WIDTH = Dimensions.get('window').width + 80;
+export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
 
 function ExampleCard({ item, index }) {
   return (
     <View style={styles.container} key={index}>
-      <Image
-        source={{ uri: item.imgUrl }}
-        style={styles.image}
-      />
-      <Text style={styles.header}>{item.title}</Text>
-      <Text style={styles.body}>{item.body}</Text>
+      <Text style={styles.header}>{item.type}</Text>
+      <Text style={styles.body}>numbersapi.com/{item.number}/{item.type}</Text>
+      <Text style={styles.body}>{item.message}</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -22,6 +19,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     borderRadius: 8,
     width: ITEM_WIDTH,
+    height: 300,
     paddingBottom: 40,
     shadowColor: "#000",
     shadowOffset: {
@@ -31,10 +29,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.29,
     shadowRadius: 4.65,
     elevation: 7,
-  },
-  image: {
-    width: ITEM_WIDTH,
-    height: 300,
   },
   header: {
     color: "#222",
@@ -47,9 +41,9 @@ const styles = StyleSheet.create({
     color: "#222",
     fontSize: 18,
     paddingLeft: 20,
-    paddingLeft: 20,
+    paddingTop: 20,
     paddingRight: 20
   }
-})
+});
 
-export default ExampleCard
+export default ExampleCard;
