611be5d3b5364c624488079a0c1576252b71c1e3
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2023-01-17 12:28:35 -0800

    Replace default placeholder data with static nums_api data

--- data.js
+++ data.js
@@ -1,18 +1,25 @@
+"use strict";
+
 const data = [
   {
-    title: "Aenean leo",
-    body: "Ut tincidunt tincidunt erat. Sed cursus turpis vitae tortor. Quisque malesuada placerat nisl. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
-    imgUrl: "https://picsum.photos/id/11/200/300",
+    number: "42",
+    message: "42 is the number of little squares forming the left side trail of Microsoft's Windows 98 logo.",
+    type: "trivia",
   },
   {
-    title: "In turpis",
-    body: "Aenean ut eros et nisl sagittis vestibulum. Donec posuere vulputate arcu. Proin faucibus arcu quis ante. Curabitur at lacus ac velit ornare lobortis. ",
-    imgUrl: "https://picsum.photos/id/10/200/300",
+    number: "5",
+    message: "5 is the number of platonic solids.",
+    type: "math",
   },
   {
-    title: "Lorem Ipsum",
-    body: "Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.",
-    imgUrl: "https://picsum.photos/id/12/200/300",
+    number: "1/17",
+    message: "January 17th is the day in 1997 that a Delta 2 carrying a GPS2R satellite explodes 13 seconds after launch, dropping 250 tons of burning rocket remains around the launch pad.",
+    type: "dates",
+  },
+  {
+    number: "1280",
+    message: "1280 is the year that the final expansion of the Lincoln Cathedral is completed.",
+    type: "years",
   },
 ];
 
