3091a78e5c1065e335d4c91c31c02fe779509c57
Philip Dimaano <pdimaano7@gmail.com>
2023-01-17 10:11:32 -0800

    Created ExampleCard component, successfully renders in iOS emulator

---
+++ ExampleCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function ExampleCard() {
+  return (
+    <View >
+      <Text>Try editing me! 🎉</Text>
+    </View>
+  );
+};
+
+export default ExampleCard;
