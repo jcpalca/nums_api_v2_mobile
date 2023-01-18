ec3ce58fb7ef55ad85643e6eb4e013ef0a88c7df
Philip Dimaano <pdimaano7@gmail.com>
2023-01-18 15:26:08 -0800

    Placeholder for API documentation in the future

---
+++ Documentation.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function Documentation() {
+  return (
+    <View>
+      <Text>Documentation</Text>
+    </View>
+  );
+}
+
+export default Documentation;
