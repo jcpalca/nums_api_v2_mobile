ba339b0c68d9681d4f707b31e38e3455b0c0928b
Philip Dimaano <pdimaano7@gmail.com>
2023-01-18 15:23:16 -0800

    Refactored to include side view menu

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
-import { StyleSheet, ScrollView } from "react-native";
-import ExampleCarousel from "./ExampleCarousel";
-import InteractiveTable from "./InteractiveTable";
+import * as React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import DrawerItems from "./DrawerItems";
+import ExampleCarousel from "./ExampleCarousel"
+import InteractiveTable from "./InteractiveTable"
+import Documentation from "./Documentation"
+
+const Drawer = createDrawerNavigator();
 
 /** App: Top level component rendering ExampleCarousel
  *
@@ -13,21 +21,46 @@ import InteractiveTable from "./InteractiveTable";
 
 export default function App() {
   console.debug("App");
-
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <ExampleCarousel />
-      <InteractiveTable />
-    </ScrollView>
+    <NavigationContainer>
+      <Drawer.Navigator
+        drawerType="front"
+        initialRouteName="Carousel"
+        screenOptions={{
+          activeTintColor: "#e91e63",
+          itemStyle: { marginVertical: 10 },
+        }}
+      >
+        {DrawerItems.map((drawer) => (
+          <Drawer.Screen
+            key={drawer.name}
+            name={drawer.name}
+            options={{
+              drawerIcon: ({ focused }) =>
+                drawer.iconType === "MaterialIcons" ? (
+                  <MaterialIcons
+                    name={drawer.iconName}
+                    size={24}
+                    color={focused ? "#e91e63" : "black"}
+                  />
+                ) : (
+                  <SimpleLineIcons
+                    name={drawer.iconName}
+                    size={24}
+                    color={focused ? "#e91e63" : "black"}
+                  />
+                ),
+            }}
+            component={
+              drawer.name === "Carousel"
+                ? ExampleCarousel
+                : drawer.name === "Try It Out!"
+                ? InteractiveTable
+                : Documentation
+            }
+          />
+        ))}
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 0.65,
-    paddingBottom: 300,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
