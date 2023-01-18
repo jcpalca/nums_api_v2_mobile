ff64264a58e5706f5fcd879f55115cd675dacd14
Philip Dimaano <pdimaano7@gmail.com>
2023-01-18 15:26:36 -0800

    File to render sections in drawer side menu

---
+++ DrawerItems.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    name: 'Carousel',
+    iconType: 'MaterialIcons',
+    iconName: 'view-carousel'
+  },
+  {
+    name: 'Try It Out!',
+    iconType: 'MaterialIcons',
+    iconName: 'input'
+  },
+  {
+    name: 'API Docs',
+    iconType: 'SimpleLineIcons',
+    iconName: 'docs'
+  }
+]
