28f15be0e0bb981a5cd7645ceac621ea37dec1f1
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2023-01-17 17:03:39 -0800

    Create InteractiveTable component

---
+++ InteractiveTable.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { View } from "react-native";
+import { getFact } from "./utils";
+
+function InteractiveTable() {
+  console.debug('InteractiveTable');
+
+  const [fact, setFact] = useState({
+    "number": "42",
+    "type": "trivia"
+  })
+
+  console.log('fact', fact);
+
+  const [currSearch, setCurrSearch] = useState();
+  console.log('currSearch', currSearch);
+
+  useEffect(function fetchFact() {
+    async function fetchFactAPI() {
+      const factInfo = await getFact(fact.number, fact.type);
+      setFact(factInfo);
+    }
+
+    fetchFactAPI();
+  }, []);
+
+  async function searchNum(endpoint) {
+    const [cat, arg1, arg2] = endpoint.split("/");
+    let factInfo;
+
+    if(cat === "dates") {
+      factInfo = await getFact(`${arg1}/${arg2}`, cat);
+    }
+    else {
+      factInfo = await getFact(arg1, cat)
+    }
+
+    setFact(factInfo);
+    setCurrSearch(endpoint);
+  }
+
+  async function searchRand(endpoint) {
+    const [cat, arg1] = endpoint.split("/");
+    console.log("RANDOM SEARCH", cat, arg1);
+    let factInfo = await getFact(arg1, cat);
+    factInfo.number = "random";
+    setFact(factInfo);
+  }
+
+  return (
+    <View>
+      <InteractiveCard />
+      <RandomCard />
+    </View>
+  )
+}
