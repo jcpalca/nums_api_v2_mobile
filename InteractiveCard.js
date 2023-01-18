8d6fffc042e38b32eeb2c202b3659c3939506d4f
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2023-01-18 12:30:44 -0800

    Update endpoint based on text input

--- InteractiveCard.js
+++ InteractiveCard.js
@@ -1,18 +1,43 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, TextInput } from "react-native";
 
-const InteractiveCard = () => {
-  const [text, onChangeText] = React.useState("");
+function InteractiveCard({ fact, searchNum }) {
+  let endpoint = "";
+
+  if (fact.number === "random") {
+    endpoint = `${fact.type}/random`;
+  }
+  else if (fact.type === "dates") {
+    endpoint = `${fact.type}/${fact.month}/${fact.day}`;
+  }
+  else if (fact.type === "years" || fact.type === "math" || fact.type === "trivia") {
+    endpoint = `${fact.type}/${fact.number}`;
+  }
+  else {
+    endpoint = "";
+  }
+
+  console.log("ENDPOINT", endpoint);
+
+  const [text, setText] = React.useState(endpoint);
+
+  console.log("TEXT", text);
+
+  function onChangeText(value) {
+    setText(value);
+    searchNum(value);
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>numbersapi.com/</Text>
+      <Text style={styles.text}>numbersapi.com/api/</Text>
       <TextInput
         style={styles.input}
         onChangeText={onChangeText}
-        placeholder={""}
+        placeholder={"trivia/random"}
         value={text}
       />
+      <Text style={styles.result}>{fact.message}</Text>
     </SafeAreaView>
   );
 };
@@ -28,6 +53,12 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
   },
+  result: {
+    width: 300,
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 30
+  }
 });
 
 export default InteractiveCard;
