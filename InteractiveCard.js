41c1e974d3a7da5cb79173ebf14c81af9193bb34
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2023-01-19 10:18:09 -0800

    Add docstring and a console debug on render

--- InteractiveCard.js
+++ InteractiveCard.js
@@ -1,7 +1,39 @@
-import React from "react";
+import { useState } from "react";
 import { SafeAreaView, StyleSheet, Text, TextInput } from "react-native";
 
+/** InteractiveCard: Renders a text input and a message generated from
+ *                   numbers_api based on the input
+ *
+ *  Props: fact: obj like:
+ *    ERROR:
+      {
+        "message": f"A math fact for { number } not found"
+        "status": 404
+      }
+
+      MATH/TRIVIA/YEAR:
+      {
+        "number": "145"
+        "message": "145 is the atomic number of Unquadpentium."
+        "type": "math"
+      }
+
+      DATE:
+      {
+        "month": "1"
+        "day": "1"
+        "dayOfYear": 1
+        "message": "January 1st is the test case.",
+        "type": "date"
+      }
+ *        searchNum: function to be called in parent component
+ *
+ *  InteractiveTable -> InteractiveCard
+ */
+
 function InteractiveCard({ fact, searchNum }) {
+  console.debug("InteractiveCard")
+
   let endpoint = "";
 
   if (fact.number === "random") {
@@ -19,7 +51,7 @@ function InteractiveCard({ fact, searchNum }) {
 
   console.log("ENDPOINT", endpoint);
 
-  const [text, setText] = React.useState(endpoint);
+  const [text, setText] = useState(endpoint);
 
   console.log("TEXT", text);
 
